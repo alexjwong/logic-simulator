f68b001f5ea28cf5aa69388dc760f9d14769aac1
tarawinn <tarawinn@bu.edu>
2014-04-27 19:21:09 -0400

    Update and.cpp

--- and.cpp
+++ and.cpp
@@ -4,8 +4,9 @@
 
 AND::AND(Gate in1, Gate in2) : Gate(){
 	std::cout << "AND gate constructed" << std::endl;
-	input1 = in1.get_output;
-	input2 = in2.get_output;
+	input1 = in1.output;
+	input2 = in2.output;
+	output = input1 & input2;
 
 	// Determine tier
 	if (in1.tier >= in2.tier){
@@ -14,7 +15,3 @@ AND::AND(Gate in1, Gate in2) : Gate(){
 	else tier = in2.tier + 1;
 }	
 
-int AND::get_output()
-{
-	return input1 & input2;
-}
