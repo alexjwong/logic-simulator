79b4614d3d18f787584d508c4e5c2824f85c54b2
tarawinn <tarawinn@bu.edu>
2014-04-27 19:24:20 -0400

    Update nor.cpp

--- nor.cpp
+++ nor.cpp
@@ -1,19 +1,16 @@
 #include "nor.h"
 
 NOR::NOR(Gate in1, Gate in2) : Gate(){
-	input1 = in1.get_output();
-	input2 = in2.get_output();
+	input1 = in1.output;
+	input2 = in2.output;
+	output = ~(input1 | input2);
 
 	// Determine tier
 	if (in1.tier >= in2.tier)
 	{
 		tier = in1.tier + 1;
 	}
-	else tier = in2.tier +1;
+	else tier = in2.tier + 1;
 
 }
 
-int NOR::get_output()
-{
-	return ~(input1 | input2);
-}
