5ab27fb9e1c89fc9411f5ce67faf6418597e42aa
tarawinn <tarawinn@bu.edu>
2014-04-27 19:35:26 -0400

    Update not.cpp

--- not.cpp
+++ not.cpp
@@ -1,14 +1,12 @@
 #include "not.h"
 
-NOT::NOT(Gate in1, Gate in2) : Gate(){
+NOT::NOT(Gate in1)
+{
 	input1 = in1.output;
 	output = ~input1;
 
 	// Determine tier
-	if (in1.tier >= in2.tier){
-		tier = in1.tier + 1;
-	}
-	else tier = in2.tier +1;
+	tier = in1.tier + 1;
 
 }
 
