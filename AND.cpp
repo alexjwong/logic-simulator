80769bed8363091c6fe8838512772a0a0c19154b
dereckf <dereckf@bu.edu>
2014-04-30 18:27:43 -0400

    Update AND.cpp

--- AND.cpp
+++ AND.cpp
@@ -2,13 +2,5 @@
 
 AND::AND() : Gate()
 {
-	input1 = in1.output;
-	input2 = in2.output;
 	output = input1 & input2;
-
-	// Determine tier
-	if (in1.tier >= in2.tier){
-		tier = in1.tier + 1;
-	}
-	else tier = in2.tier + 1;
 }
