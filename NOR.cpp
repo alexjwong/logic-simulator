4e276ae5860fc451fd25b09287f07e0843f4ebe4
tarawinn <tarawinn@bu.edu>
2014-04-30 20:23:03 -0400

    Update NOR.cpp

--- NOR.cpp
+++ NOR.cpp
@@ -1,16 +1,16 @@
 #include "NOR.h"
 
-NOR::NOR(Gate in1, Gate in2) : Gate(){
-	input1 = in1.output;
-	input2 = in2.output;
-	output = ~(input1 | input2);
 
-	// Determine tier
-	if (in1.tier >= in2.tier)
-	{
-		tier = in1.tier + 1;
-	}
-	else tier = in2.tier + 1;
+NOR::NOR() : Gate(){
+	std::cout << "NOR gate constructed" << std::endl;
+}
 
+void NOR::set_linkage(Gate* g1, Gate* g2){
+	input1 = g1;
+	input2 = g2;
+}
+
+bool NOR::get_output(){
+	return ~(input1.get_output() | input2.get_output());
 }
 
