46d172051355f988a1293b36ba1dfdc98c1e7c34
tarawinn <tarawinn@bu.edu>
2014-04-30 20:28:21 -0400

    Update XOR.cpp

--- XOR.cpp
+++ XOR.cpp
@@ -1,15 +1,15 @@
 #include "XOR.h"
 
-XOR::XOR(Gate in1, Gate in2) : Gate(){
-	input1 = in1.output;
-	input2 = in2.output;
-	output = input1 ^ input2;
 
-	// Determine tier
-	if (in1.tier >= in2.tier){
-		tier = in1.tier + 1;
-	}
-	else tier = in2.tier +1;
-
+XOR::XOR() : Gate(){
+	std::cout << "XOR gate constructed" << std::endl;
 }
 
+void XOR::set_linkage(Gate* g1, Gate* g2){
+	input1 = g1;
+	input2 = g2;
+}
+
+bool XOR::get_output(){
+	return (input1.get_output() ^ input2.get_output());
+}
