ef65e4b4f873b17e8656e9985e2ccd6a56932831
Alex Wong <alexwong@bu.edu>
2014-04-30 20:16:04 -0400

    New structure for gates

--- AND.h
+++ AND.h
@@ -1,14 +1,20 @@
 #ifndef AND_H
 #define AND_H
 
-#include "gate.h"
+#include "Gate.h"
 
 class Gate;
 
 class AND: public Gate{
 	public:
 		AND();
-	
+
+		void set_linkage(Gate* g1, Gate* g2);
+		bool get_output();
+
+	private:
+		Gate* input1;
+		Gate* input2;
 };
 
 #endif
