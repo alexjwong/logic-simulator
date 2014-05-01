22c1adf8037fe3a566ce5941f9272a3388e16938
dereckf <dereckf@bu.edu>
2014-04-30 20:52:03 -0400

    Update OR.cpp

--- OR.cpp
+++ OR.cpp
@@ -11,5 +11,5 @@ void OR::set_linkage(Gate* g1, Gate* g2){
 }
 
 bool OR::get_output(){
-	return (input1.get_output() | input2.get_output());
+	return (input1->get_output() | input2->get_output());
 }
