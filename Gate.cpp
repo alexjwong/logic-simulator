8cc199912ba45877643a102b24304fceae09ffdd
dereckf <dereckf@bu.edu>
2014-05-01 22:17:02 -0400

    Update Gate.cpp

--- Gate.cpp
+++ Gate.cpp
@@ -2,10 +2,12 @@
 
 Gate::Gate(){
 	std::cout << "Gate constructed." << std::endl;
+	gate_count++;
 }
 
 Gate::~Gate(){
 	std::cout << "Gate destructed." << std::endl;
+	gate_count--;
 }
 
 bool Gate::get_output(){
