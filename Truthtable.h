a9255f64b9823419a2d97abe68d93de688d8b923
Alex Wong <alexwong@bu.edu>
2014-05-06 17:22:02 -0400

    Work on Truthtable function

---
+++ Truthtable.h
@@ -0,0 +1,4 @@
+#include <string>
+
+// Returns the truthtable for given linkage in string form.
+string generate_truthtable();
--- truthtable.cpp
+++ truthtable.cpp
@@ -16,100 +16,8 @@
 #include <sstream>
 using namespace std;
 
-string generate_truthtable() {
-    
-	//bool result;
-    
-	//Input* x = new Input;
-	//Input* y = new Input;
-    
-	
-    
-	
-    
-	/*
-     x->set_output(1);
-     y->set_output(1);
-     */
-    
-	//result = o->get_output();
-    
-	//std::cout << result << std::endl;
-    
-    
-	/*int truthtable[4][3];
-     
-     truthtable[0][0] = 0;
-     truthtable[0][1] = 0;
-     truthtable[1][0] = 0;
-     truthtable[1][1] = 1;
-     truthtable[2][0] = 1;
-     truthtable[2][1] = 0;
-     truthtable[3][0] = 1;
-     truthtable[3][1] = 1;
-     
-     int count = 0;
-     
-     for(int i=0;i<=1;i++){
-     for(int j=0;j<=1;j++){
-     x->set_output(i);
-     y->set_output(j);
-     
-     truthtable[count][2] = o->get_output();
-     count++;
-     
-     }
-     }
-     
-     for(int k=0;k<4;k++){
-     for (int l=0;l<3;l++){
-     std::cout << truthtable[k][l];
-     }
-     std::cout << std::endl;
-     }
-     
-     
-     
-     delete x;
-     delete y;
-     delete gate1;
-     delete o;
-     
-     
-     */
-    
-    Input* a = new Input;
-    Input* b = new Input;
-    Input* c = new Input;
-    Input* d = new Input;
-    //Input* e = new Input;
-    //Input* f = new Input;
-    //Input* g = new Input;
-    
-    ///////////////////////
-    ///////TESTING/////////
-    ///////////////////////
-    OR* gate1 = new OR;
-    AND* gate2 = new AND;
-    NOR* gate3 = new NOR;
-    gate2->set_linkage(gate1,c);
-    gate1->set_linkage(a,b);
-    gate3->set_linkage(d, gate2);
-    Output* o = new Output;
-    Output* o2= new Output;
-    o->set_linkage(gate2);
-    o2->set_linkage(gate3);
-    Input * input_ptrs[Input::num_inputs];
-    Output * output_ptrs[Output::num_outputs];
-    input_ptrs[0]=a;
-    input_ptrs[1]=b;
-    input_ptrs[2]=c;
-    input_ptrs[3]=d;
-    output_ptrs[0]=o;
-    output_ptrs[1]=o2;
-    ///////////////////////
-    
-    
+string generate_truthtable(input_ptrs[]) {
+       
     int inamount = Input::num_inputs;
     int outamount = Output::num_outputs;
     const int row = pow(2,inamount);
@@ -325,7 +233,7 @@ string generate_truthtable() {
     }
     
     
-    /*
+    /*  Prints the truthtable
     for (int i=0;i<row;i++)
     {
         for (int j=0;j<column;j++)
@@ -335,17 +243,9 @@ string generate_truthtable() {
         cout<<endl;
     }
     
-*/
-   
-    
-    string poop;
-    /* 
-    
-    stringstream ss;
-    ss<<"Space Complexity: ";
-    ss<< ((Gate::gate_count)-(Input::num_inputs)-(Output::num_outputs));
-    ss<<"\n";
     */
+   
+    string poop;
     
     for (int i=0;i<row;i++)
     {
